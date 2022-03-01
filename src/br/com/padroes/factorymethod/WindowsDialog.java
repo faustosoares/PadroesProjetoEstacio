7e7d140428f806626d69c04d6d191804ec168a17
Fausto Bernard Melo Soares <fausto.ifs@gmail.com>
2022-03-01 11:39:28 -0300

    Commit Inicial

---
+++ src/br/com/padroes/factorymethod/WindowsDialog.java
@@ -0,0 +1,12 @@
+package br.com.padroes.factorymethod;
+
+/**
+ * Windows Dialog will produce Windows buttons.
+ */
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
